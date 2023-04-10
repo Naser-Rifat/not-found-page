599f8a347d7e2d413dabd8125ca086a0b89aaa39
Naser-Rifat <rifatmahmud971@gmail.com>
2023-04-11 00:16:29 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { NotFoundPage } from "./Pages/NotFoundPage";
+
+function App() {
+  return (
+    <div>
+      <NotFoundPage />
+    </div>
+  );
+}
+
+export default App;
