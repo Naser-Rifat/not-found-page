139fc43486fa2babedbd95ceb987038bf07dcd55
Naser-Rifat <rifatmahmud971@gmail.com>
2023-04-12 00:08:44 +0600

    update

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -14,14 +14,18 @@ function HomePage() {
 
       <div className="-mt-64 ">
         <div className="w-full text-center">
-          <p className="text-sm tracking-widest text-white">Subtitle</p>
-          <h1 className="font-bold text-5xl text-white">Title</h1>
+          <h1 className="font-bold text-5xl text-white">
+            B-Code 404 Error pages
+          </h1>
+          <p className="text-md tracking-widest text-white mt-3 font-sans">
+            Experience the power of professional website pages with B-code.
+          </p>
         </div>
 
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 ">
           <div className="p-2 sm:p-10 text-center cursor-pointer">
             <Link to={"404-page-01"}>
-              <div className="p-0 h-80 max-w-sm  rounded overflow-hidden border border-gray-300 shadow-lg hover:bg-white transition duration-500  bg-white">
+              <div className="p-0 h-80 max-w-sm  rounded  overflow-hidden border border-gray-300 shadow-lg hover:bg-white hover:scale-105 transition duration-500  bg-white">
                 <img
                   className="block w-full h-full object-cover"
                   src={notFoundImg01}
@@ -32,7 +36,7 @@ function HomePage() {
           </div>
           <div className="p-2 sm:p-10 text-center cursor-pointer">
             <Link to={"404-page-03"}>
-              <div className="p-0 h-80 max-w-sm  rounded overflow-hidden border border-gray-300 shadow-lg hover:bg-white transition duration-500  bg-white">
+              <div className="p-0 h-80 max-w-sm  rounded  hover:scale-105 overflow-hidden border border-gray-300 shadow-lg hover:bg-white transition duration-500  bg-white">
                 <img
                   className="block w-full h-full object-cover"
                   src={notFoundImg03}
@@ -43,7 +47,7 @@ function HomePage() {
           </div>
           <div className="p-2 sm:p-10 text-center cursor-pointer">
             <Link to={"404-page-02"}>
-              <div className="p-0 h-80 max-w-sm  rounded overflow-hidden border border-gray-300 shadow-lg hover:bg-white transition duration-500  bg-white">
+              <div className="p-0 h-80 max-w-sm  rounded overflow-hidden border  hover:scale-105 border-gray-300 shadow-lg hover:bg-white transition duration-500  bg-white">
                 <img
                   className="block w-full h-full object-cover"
                   src={notFoundImg02}
