2337c75a2658043aa7cdea56dfd53dada3a81f49
Naser-Rifat <rifatmahmud971@gmail.com>
2023-04-12 19:35:09 +0600

    update

--- src/Pages/NotFoundPage03.jsx
+++ src/Pages/NotFoundPage03.jsx
@@ -13,7 +13,7 @@ export const NotFoundPage03 = () => {
           <span class="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
 
           <span class="relative block px-8 py-3 bg-[#1A2238] border border-current">
-            <Link to="/">Go Home</Link>
+            <a href="/">Go Home</a>
           </span>
         </a>
       </button>
